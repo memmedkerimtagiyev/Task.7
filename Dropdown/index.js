c735b06f17bdc50f46d51ea495c3a084727f84bc
memmedkerim taghiyev <7m16zu6@code.edu.az>
2023-05-15 16:25:51 +0300

    added

---
+++ Dropdown/index.js
@@ -0,0 +1,32 @@
+const bar = document.querySelector(".icon-bar")
+const Xicon = document.querySelector("#X-icon")
+const respList = document.querySelector("#responsive-list")
+const iconArrow = document.querySelector(".icon-arrow")
+const iconArrow2 = document.querySelector(".icon-arrow2")
+const opacty = document.querySelector(".opacty");
+
+bar.addEventListener(("click"), () => {
+    respList.style.width = "300px"
+    console.log("salam")
+    opacty.style.display = "block";
+})
+Xicon.addEventListener(("click"), () => {
+    respList.style.width = "0px"
+    console.log("salam")
+    opacty.style.display = "none";
+})
+iconArrow.addEventListener(("click"), () => {
+    console.log("salam")
+    let add = document.getElementById("dropOne-res")
+    console.log(add)
+    add.classList.toggle("open")
+    let trans=document.getElementById("ox")
+    trans.classList.toggle("transform")
+})
+iconArrow2.addEventListener(("click"), () => {
+    console.log("salam")
+    let add2 = document.getElementById("dropTwo-res")
+    add2.classList.toggle("open")
+    let trans2=document.getElementById("ox2")
+    trans2.classList.toggle("transform")
+})
