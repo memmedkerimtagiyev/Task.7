322d3c58443059592a3bb3a49ae6d1392a7cc273
memmedkerimtagiyev <7M16ZU6@code.edu.az>
2023-05-25 10:05:22 +0400

    sak3

--- testImtahan3/assets/js/index.js
+++ testImtahan3/assets/js/index.js
@@ -1,13 +1,41 @@
 const barIcon = document.querySelector(".barIcon");
-const Xicon=document.querySelector(".Xicon")
+const Xicon = document.querySelector(".Xicon");
 const clickBarIcon = document.querySelector(".clickBarIcon");
 
+barIcon.addEventListener("click", () => {
+  console.log("salam");
+  clickBarIcon.classList.toggle("barIconNav");
+});
 
-barIcon.addEventListener("click",()=>{
-    console.log("salam")
-    clickBarIcon.classList.toggle("barIconNav")
-})
+Xicon.addEventListener("click", () => {
+  clickBarIcon.classList.remove("barIconNav");
+});
 
-Xicon.addEventListener(("click"),()=>{
-clickBarIcon.classList.remove("barIconNav")
-})
+const recentCards = document.querySelector(".recentCards");
+
+fetch("./assets/json/index.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((element) => {
+      console.log(element);
+
+      const recentCard = document.createElement("div");
+      const foto = document.createElement("img");
+      const tarix = document.createElement("span");
+      const paraqraf = document.createElement("p");
+
+      recentCard.appendChild(foto);
+      recentCard.appendChild(tarix);
+      recentCard.appendChild(paraqraf);
+      recentCards.appendChild(recentCard);
+
+      recentCard.className = "recentCard";
+      foto.tagName = "img";
+      paraqraf.tagName = "p";
+      tarix.tagName = "span";
+
+      foto.innerText = element.img;
+      tarix.innerText=element.date
+      paraqraf.innerText=element.ressons
+    });
+  });
